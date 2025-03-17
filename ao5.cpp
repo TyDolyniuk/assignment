a8a1e6fcad218e4453b74d4b052cd14ad74f0fa2
TyDolyniuk <ty.dolyniuk@pcc.edu>
2025-03-17 00:53:58 +0000

    Finished

--- ao5.cpp
+++ ao5.cpp
@@ -2,9 +2,11 @@
 # Author:           Ty Dolyniuk
 # Assignment:       A5 (CS161A)
 # Date:             March 15, 2025
-# Description:      
-# Input:            
-# Output:           
+# Description:      Program will ask for three inputs that will be used to
+#                   calculate the statistics for amount of people, total cost,
+#                   and averages.
+# Input:            int numPeople, float avgSlicePerPerson, costPerPizza, char c
+# Output:           int numberEntries, totalPizzaCoumt, maxPeople, float maxCost
 # Sources:          Assignment 5 specifications, textbook
 #******************************************************************************/
 
@@ -20,46 +22,66 @@ int main() {
 
     bool next = true;
 
+    cout << "Welcome to my Pizza Party Statistics program!" << endl;
+
     while(next) {
         int numPeople = 0;
         float avgSlicePerPerson = 0.0;
         float costPerPizza = 0.0;
 
-        cout << "Welcome to my Pizza Party Statistics program!" << endl;
         cout << endl << "Enter the number of people, average number of slices per person," << endl;
         cout << "and the cost of a pizza separated by a space: ";
         cin >> numPeople >> avgSlicePerPerson >> costPerPizza;
 
-        int numberOfPizzas = ceil((numPeople * avgSlicePerPerson) / 8);
-        float costOfPizzas = numberOfPizzas * costPerPizza;
-        float tax = costOfPizzas * 0.07;
-        float deliveryCost = (costOfPizzas + tax) * 0.2;
-        float totalCost = costOfPizzas + tax + deliveryCost;
+        if (numPeople <= 0 || avgSlicePerPerson <= 0 || costPerPizza <= 0) {
+            cout << endl << "ERROR! Values must be greater than 0!" << endl;
+        }
+        else {
+            int numberOfPizzas = ceil((numPeople * avgSlicePerPerson) / 8);
+            float costOfPizzas = numberOfPizzas * costPerPizza;
+            float tax = costOfPizzas * 0.07;
+            float deliveryCost = (costOfPizzas + tax) * 0.2;
+            float totalCost = costOfPizzas + tax + deliveryCost;
 
-        numberEntries += 1;
+            cout << fixed << setprecision(2);
 
-        totalPizzaCount += numberOfPizzas;
-        if(numPeople > maxPeople) {
-            maxPeople = numPeople;
+            numberEntries += 1;
+
+            totalPizzaCount += numberOfPizzas;
+            if(numPeople > maxPeople) {
+                maxPeople = numPeople;
+            }
+
+            if(totalCost > maxCost) {
+                maxCost = totalCost;
+            }
+
+            cout << endl << setw(20) << left << "Number of pizzas:";
+            cout << setw(10) << right << numberOfPizzas << endl;
+            cout << setw(20) << left << "Cost of pizzas:";
+            cout << setw(10) << right << "$" << costOfPizzas << endl;
+            cout << setw(20) << left << "Tax:";
+            cout << setw(10) << right << "$" << tax << endl;
+            cout << setw(20) << left << "Delivery:";
+            cout << setw(10) << right << "$" << deliveryCost << endl;
+            cout << setw(20) << left << "Total Cost:";
+            cout << setw(10) << right << "$" << totalCost << endl;
         }
 
-        if(totalCost > maxCost) {
-            maxCost = totalCost;
-        }
-
-        cout << "Do you want to enter more (y/n): ";
+        cout << endl << "Do you want to enter more (y/n): ";
         char c;
         cin >> c;
-        if(c != 'y') {
+        if(tolower(c) != 'y') {
             next = false;
         }
     }
 
-    cout << "Number of entries: " << numberEntries << endl;
-    cout << "Average number of pizzas: " << (totalPizzaCount / static_cast<float>(numberEntries)) << endl;
+    cout << endl << "Number of entries: " << numberEntries << endl;
+    cout << "Total number of pizzas: " << totalPizzaCount << endl;
+    cout << fixed << setprecision(1) << "Average number of pizzas: " << (totalPizzaCount / static_cast<float>(numberEntries)) << endl;
     cout << "Maximum number of people: " << maxPeople << endl;
-    cout << "Maximum cost of pizzas: " << maxCost << endl;
+    cout << fixed << setprecision(2) << "Maximum cost of pizzas: $" << maxCost << endl;
 
-    cout << "Thank you for using my program!" << endl;
+    cout << endl << "Thank you for using my program!" << endl;
     return 0;
 }
